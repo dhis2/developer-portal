cf9f467a3d953946ac7549aa89c874b7f4a96c87
Debora Galeano <dgaleanoservian@gmail.com>
2021-07-20 16:46:47 +0200

    docs[events]: add meetup page (#36)

--- sidebarsEvents.js
+++ sidebarsEvents.js
@@ -6,6 +6,7 @@ module.exports = {
         label: 'Developer Academy',
         items: ['academy-workshops-2021', 'academy-workshops-2020'],
       },
+      'developer-meetups', 
       'annual-conference',
     ],
   };
