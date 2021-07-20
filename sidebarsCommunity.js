f15f085d1d62056cb4d669e8f0b0b1b19b8e1c88
Debora Galeano <dgaleanoservian@gmail.com>
2021-07-20 16:41:41 +0200

    docs: improve community page  (#35)
    * redo community page ad move contributing
    
    * add meetup events

---
+++ sidebarsCommunity.js
@@ -0,0 +1,11 @@
+module.exports = {
+    events: [
+      'support', 
+      'stay-connected',
+      {
+        type: 'category',
+        label: 'Contributing',
+        items: ['contribute-dev-portal', 'contribute'],
+      },
+    ],
+  };
