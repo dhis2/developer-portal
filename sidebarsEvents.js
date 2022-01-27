61696d06537031579fc08d793a24399aec22a087
Austin McGee <947888+amcgee@users.noreply.github.com>
2022-01-27 15:23:20 +0100

    chore: make academies title plural

--- sidebarsEvents.js
+++ sidebarsEvents.js
@@ -3,7 +3,7 @@ module.exports = {
         'webinars',
         {
             type: 'category',
-            label: 'Developer Academy',
+            label: 'Developer Academies',
             items: ['academy-workshops-2021', 'academy-workshops-2020'],
         },
         'developer-meetups',
