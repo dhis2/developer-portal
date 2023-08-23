fa575babb750e3856b739d1dcd9e33b573915a1b
Rene Pot <rene@topener.nl>
2023-08-23 13:40:20 +0200

    feat: update docs to pull from cli repo

---
+++ docs-migrate.js
@@ -0,0 +1,20 @@
+const { execSync } = require('child_process')
+const fs = require('fs-extra')
+
+try {
+    // Clone the repository and switch to the "docs-improve" branch
+    execSync(
+        'git clone --depth 1 git@github.com:dhis2/cli.git --branch docs-improve .cli-repo-temp'
+    )
+
+    // Remove any previous copy
+    fs.removeSync('./docs/cli')
+
+    // Copy the directory to another place and create missing directories
+    fs.copySync('.cli-repo-temp/docs', './docs/cli', { recursive: true })
+
+    // Remove the checked out code
+    fs.removeSync('.cli-repo-temp')
+} catch (error) {
+    console.error(error)
+}
--- sidebars.js
+++ sidebars.js
@@ -1,42 +1,72 @@
 module.exports = {
     docs: [
-        {'Quick Start': [
-            'quickstart',
-            'quickstart/quickstart-web',
-            'quickstart/quickstart-android',
-        ]},
-        {Tutorials: [
-            'tutorials',
-            'tutorials/setup-env',
-            'tutorials/dhis2-docker',
-            'tutorials/ui-library',
-            {
-                'DHIS2 App Runtime': [
-                    'tutorials/app-runtime-query',
-                    'tutorials/app-runtime-mutation',
-                ],
-            },
-        ]},
-        {Guides: [
-            'guides',
-            {
-                'Local Development': [
-                    'guides/spin-up-local-instance',
-                    'guides/code-style',
-                    'guides/debug-instance',
-                ],
-                'UI Library': ['guides/ui-table'],
-                'DHIS2 App Runtime': ['guides/query-playground'],
-                'App Hub': [
-                    'guides/submit-apphub',
-                    'guides/apphub-guidelines',
-                    'guides/publish-apphub',
-                ],
-                Translation: ['guides/translation-support'],
-            },
-        ]},
+        {
+            'Quick Start': [
+                'quickstart',
+                'quickstart/quickstart-web',
+                'quickstart/quickstart-android',
+            ],
+        },
+        {
+            Tutorials: [
+                'tutorials',
+                'tutorials/setup-env',
+                'tutorials/dhis2-docker',
+                'tutorials/ui-library',
+                {
+                    'DHIS2 App Runtime': [
+                        'tutorials/app-runtime-query',
+                        'tutorials/app-runtime-mutation',
+                    ],
+                },
+            ],
+        },
+        {
+            CLI: [
+                'cli/getting-started',
+                {
+                    Commands: [
+                        'cli/commands/d2',
+                        'cli/commands/d2-cluster',
+                        'cli/commands/d2-utils-release',
+                    ],
+                    'D2 Cluster': [
+                        'cli/recipes/custom-dhis-config',
+                        'cli/recipes/custom-image',
+                        'cli/recipes/development',
+                        'cli/recipes/stable',
+                    ],
+                },
+            ],
+        },
+        {
+            Guides: [
+                'guides',
+                {
+                    'Local Development': [
+                        'guides/spin-up-local-instance',
+                        'guides/code-style',
+                        'guides/debug-instance',
+                    ],
+                    'UI Library': ['guides/ui-table'],
+                    'DHIS2 App Runtime': ['guides/query-playground'],
+                    'App Hub': [
+                        'guides/submit-apphub',
+                        'guides/apphub-guidelines',
+                        'guides/publish-apphub',
+                    ],
+                    Translation: ['guides/translation-support'],
+                },
+            ],
+        },
         'dev-videos',
         'reference',
-        {Conceptual: ['conceptual', 'conceptual/git-workflow', 'conceptual/jira-workflow']},
+        {
+            Conceptual: [
+                'conceptual',
+                'conceptual/git-workflow',
+                'conceptual/jira-workflow',
+            ],
+        },
     ],
 }
