fa9fcbb5c6bf461b9fd122e80701d7c9d5f0f269
Rene Pot <rene@topener.nl>
2023-10-05 16:34:22 +0200

    feat: add cypress cli to sidebar

--- sidebars.js
+++ sidebars.js
@@ -32,8 +32,50 @@ module.exports = {
                         'cli/cli/recipes/development-version',
                         'cli/cli/recipes/custom-dhis-config',
                         'cli/cli/recipes/custom-docker-image',
+                        'cli/cli/commands/d2-utils-release',
                     ],
-                    'd2 utils': ['cli/cli/commands/d2-utils-release'],
+                },
+            ],
+            Cypress: [
+                {
+                    id: 'cli/cypress/getting-started',
+                    type: 'doc',
+                    label: 'Getting Started',
+                },
+                'cli/cypress/upgrade-guide',
+                {
+                    Guides: [
+                        'cli/cypress/guides/setting-up-cli-tool',
+                        'cli/cypress/guides/enable-auto-login',
+                        'cli/cypress/guides/using-cucumber',
+                        'cli/cypress/guides/using-the-network-shim',
+                        'cli/cypress/guides/cross-site-cookies',
+                        'cli/cypress/guides/custom-data-test-syntax',
+                        'cli/cypress/guides/add-login-credentials',
+                    ],
+                    'API: Commands': [
+                        'cli/cypress/commands/getWithDataTest',
+                        'cli/cypress/commands/findWithDataTest',
+                        'cli/cypress/commands/fillInLoginForm',
+                        'cli/cypress/commands/all',
+                    ],
+                    'API: Helper Functions': [
+                        'cli/cypress/helpers',
+                        'cli/cypress/helpers/dataTestNameToSelector',
+                        'cli/cypress/helpers/parseSelectorWithDataTest',
+                    ],
+                    'API: Setup': ['cli/cypress/setups/enable-auto-login'],
+                    'Developer Docs': [
+                        'cli/cypress/developer',
+                        'cli/cypress/developer/install',
+                        'cli/cypress/developer/network-shim',
+                        'cli/cypress/developer/troubleshooting',
+                    ],
+                },
+                {
+                    id: 'cli/cypress/CHANGELOG',
+                    label: 'Changelog',
+                    type: 'doc',
                 },
             ],
             'Application Platform': [
