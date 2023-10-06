6eaed053aa49bd160b5df6d017af772c294cf8a7
Rene Pot <rene@topener.nl>
2023-10-06 16:51:49 +0200

    feat: add app-runtime to sidebar

--- sidebars.js
+++ sidebars.js
@@ -22,62 +22,26 @@ module.exports = {
             ],
         },
         {
-            CLI: [
-                'cli/readme',
-                'cli/cli/commands/d2-overview',
+            Guides: [
+                'guides',
                 {
-                    'd2 cluster': [
-                        'cli/cli/commands/d2-cluster',
-                        'cli/cli/recipes/stable-version',
-                        'cli/cli/recipes/development-version',
-                        'cli/cli/recipes/custom-dhis-config',
-                        'cli/cli/recipes/custom-docker-image',
-                        'cli/cli/commands/d2-utils-release',
+                    'Local Development': [
+                        'guides/spin-up-local-instance',
+                        'guides/code-style',
+                        'guides/debug-instance',
                     ],
+                    'UI Library': ['guides/ui-table'],
+                    'DHIS2 App Runtime': ['guides/query-playground'],
+                    'App Hub': [
+                        'guides/submit-apphub',
+                        'guides/apphub-guidelines',
+                        'guides/publish-apphub',
+                    ],
+                    Translation: ['guides/translation-support'],
                 },
             ],
-            Cypress: [
-                {
-                    id: 'cli/cypress/getting-started',
-                    type: 'doc',
-                    label: 'Getting Started',
-                },
-                'cli/cypress/upgrade-guide',
-                {
-                    Guides: [
-                        'cli/cypress/guides/setting-up-cli-tool',
-                        'cli/cypress/guides/enable-auto-login',
-                        'cli/cypress/guides/using-cucumber',
-                        'cli/cypress/guides/using-the-network-shim',
-                        'cli/cypress/guides/cross-site-cookies',
-                        'cli/cypress/guides/custom-data-test-syntax',
-                        'cli/cypress/guides/add-login-credentials',
-                    ],
-                    'API: Commands': [
-                        'cli/cypress/commands/getWithDataTest',
-                        'cli/cypress/commands/findWithDataTest',
-                        'cli/cypress/commands/fillInLoginForm',
-                        'cli/cypress/commands/all',
-                    ],
-                    'API: Helper Functions': [
-                        'cli/cypress/helpers',
-                        'cli/cypress/helpers/dataTestNameToSelector',
-                        'cli/cypress/helpers/parseSelectorWithDataTest',
-                    ],
-                    'API: Setup': ['cli/cypress/setups/enable-auto-login'],
-                    'Developer Docs': [
-                        'cli/cypress/developer',
-                        'cli/cypress/developer/install',
-                        'cli/cypress/developer/network-shim',
-                        'cli/cypress/developer/troubleshooting',
-                    ],
-                },
-                {
-                    id: 'cli/cypress/CHANGELOG',
-                    label: 'Changelog',
-                    type: 'doc',
-                },
-            ],
+        },
+        {
             'Application Platform': [
                 'app-platform/getting-started',
                 'app-platform/installation',
@@ -153,22 +117,127 @@ module.exports = {
             ],
         },
         {
-            Guides: [
-                'guides',
+            'Application Runtime': [
+                'app-runtime/getting-started',
+                'app-runtime/provider',
                 {
-                    'Local Development': [
-                        'guides/spin-up-local-instance',
-                        'guides/code-style',
-                        'guides/debug-instance',
+                    Hooks: [
+                        {
+                            id: 'app-runtime/hooks/README',
+                            label: 'Overview',
+                            type: 'doc',
+                        },
+                        'app-runtime/hooks/useConfig',
+                        'app-runtime/hooks/useDataQuery',
+                        'app-runtime/hooks/useDataMutation',
+                        'app-runtime/hooks/useDataEngine',
+                        'app-runtime/hooks/useAlert',
+                        'app-runtime/hooks/useAlerts',
+                        'app-runtime/hooks/useTimeZoneConversion',
                     ],
-                    'UI Library': ['guides/ui-table'],
-                    'DHIS2 App Runtime': ['guides/query-playground'],
-                    'App Hub': [
-                        'guides/submit-apphub',
-                        'guides/apphub-guidelines',
-                        'guides/publish-apphub',
+                },
+                {
+                    Components: [
+                        {
+                            id: 'app-runtime/components/README',
+                            label: 'Overview',
+                            type: 'doc',
+                        },
+                        'app-runtime/components/DataQuery',
+                        'app-runtime/components/DataMutation',
                     ],
-                    Translation: ['guides/translation-support'],
+                },
+                {
+                    Types: [
+                        'app-runtime/types/Config',
+                        'app-runtime/types/Query',
+                        'app-runtime/types/Mutation',
+                    ],
+                },
+                {
+                    Advanced: [
+                        {
+                            'Offline Tools': [
+                                {
+                                    id: 'app-runtime/advanced/offline/README',
+                                    label: 'Overview',
+                                    type: 'doc',
+                                },
+                                'app-runtime/advanced/offline/CacheableSections',
+                                'app-runtime/advanced/offline/useDhis2ConnectionStatus',
+                                'app-runtime/advanced/offline/useOnlineStatus',
+                            ],
+                        },
+                        'app-runtime/advanced/services',
+                        'app-runtime/advanced/DataEngine',
+                        'app-runtime/advanced/DataEngineLinks',
+                        'app-runtime/advanced/redux',
+                    ],
+                },
+                {
+                    id: 'app-runtime/CHANGELOG',
+                    label: 'Changelog',
+                    type: 'doc',
+                },
+            ],
+        },
+        {
+            CLI: [
+                'cli/readme',
+                'cli/cli/commands/d2-overview',
+                {
+                    'd2 cluster': [
+                        'cli/cli/commands/d2-cluster',
+                        'cli/cli/recipes/stable-version',
+                        'cli/cli/recipes/development-version',
+                        'cli/cli/recipes/custom-dhis-config',
+                        'cli/cli/recipes/custom-docker-image',
+                        'cli/cli/commands/d2-utils-release',
+                    ],
+                },
+            ],
+        },
+        {
+            Cypress: [
+                {
+                    id: 'cli/cypress/getting-started',
+                    type: 'doc',
+                    label: 'Getting Started',
+                },
+                'cli/cypress/upgrade-guide',
+                {
+                    Guides: [
+                        'cli/cypress/guides/setting-up-cli-tool',
+                        'cli/cypress/guides/enable-auto-login',
+                        'cli/cypress/guides/using-cucumber',
+                        'cli/cypress/guides/using-the-network-shim',
+                        'cli/cypress/guides/cross-site-cookies',
+                        'cli/cypress/guides/custom-data-test-syntax',
+                        'cli/cypress/guides/add-login-credentials',
+                    ],
+                    'API: Commands': [
+                        'cli/cypress/commands/getWithDataTest',
+                        'cli/cypress/commands/findWithDataTest',
+                        'cli/cypress/commands/fillInLoginForm',
+                        'cli/cypress/commands/all',
+                    ],
+                    'API: Helper Functions': [
+                        'cli/cypress/helpers',
+                        'cli/cypress/helpers/dataTestNameToSelector',
+                        'cli/cypress/helpers/parseSelectorWithDataTest',
+                    ],
+                    'API: Setup': ['cli/cypress/setups/enable-auto-login'],
+                    'Developer Docs': [
+                        'cli/cypress/developer',
+                        'cli/cypress/developer/install',
+                        'cli/cypress/developer/network-shim',
+                        'cli/cypress/developer/troubleshooting',
+                    ],
+                },
+                {
+                    id: 'cli/cypress/CHANGELOG',
+                    label: 'Changelog',
+                    type: 'doc',
                 },
             ],
         },
