6928ea16893405336a95fbe234edca7b203acf71
Rene Pot <rene@topener.nl>
2023-10-12 14:34:48 +0200

    docs: restructure sidebar

--- sidebars.js
+++ sidebars.js
@@ -42,202 +42,208 @@ module.exports = {
             ],
         },
         {
-            'Application Platform': [
-                'app-platform/getting-started',
-                'app-platform/installation',
-                'app-platform/bootstrapping',
+            'Platform Toolchain': [
                 {
-                    'CLI Scripts': [
+                    'Application Platform': [
+                        'app-platform/getting-started',
+                        'app-platform/installation',
+                        'app-platform/bootstrapping',
                         {
-                            id: 'app-platform/scripts',
-                            label: 'Overview',
-                            type: 'doc',
-                        },
-                        {
-                            id: 'app-platform/scripts/build',
-                            label: 'build',
-                            type: 'doc',
-                        },
-                        {
-                            id: 'app-platform/scripts/deploy',
-                            label: 'deploy',
-                            type: 'doc',
-                        },
-                        {
-                            id: 'app-platform/scripts/init',
-                            label: 'init',
-                            type: 'doc',
-                        },
-                        {
-                            id: 'app-platform/scripts/pack',
-                            label: 'pack',
-                            type: 'doc',
-                        },
-                        {
-                            id: 'app-platform/scripts/publish',
-                            label: 'publish',
-                            type: 'doc',
-                        },
-                        {
-                            id: 'app-platform/scripts/start',
-                            label: 'start',
-                            type: 'doc',
-                        },
-                        {
-                            id: 'app-platform/scripts/test',
-                            label: 'test',
-                            type: 'doc',
-                        },
-                    ],
-                },
-                {
-                    Configuration: [
-                        'app-platform/config/types',
-                        'app-platform/config/d2-config-js-reference',
-                        'app-platform/config/environment',
-                    ],
-                },
-                {
-                    Usage: [
-                        'app-platform/usage/dependencies',
-                        'app-platform/usage/css',
-                        'app-platform/usage/static-files',
-                        'app-platform/usage/app-icon',
-                    ],
-                },
-                'app-platform/pwa/pwa',
-                'app-platform/architecture',
-                'app-platform/troubleshooting',
-                'app-platform/proxy',
-                {
-                    id: 'app-platform/CHANGELOG',
-                    label: 'Changelog',
-                    type: 'doc',
-                },
-            ],
-        },
-        {
-            'Application Runtime': [
-                'app-runtime/getting-started',
-                'app-runtime/provider',
-                {
-                    Hooks: [
-                        {
-                            id: 'app-runtime/hooks/README',
-                            label: 'Overview',
-                            type: 'doc',
-                        },
-                        'app-runtime/hooks/useConfig',
-                        'app-runtime/hooks/useDataQuery',
-                        'app-runtime/hooks/useDataMutation',
-                        'app-runtime/hooks/useDataEngine',
-                        'app-runtime/hooks/useAlert',
-                        'app-runtime/hooks/useAlerts',
-                        'app-runtime/hooks/useTimeZoneConversion',
-                    ],
-                },
-                {
-                    Components: [
-                        {
-                            id: 'app-runtime/components/README',
-                            label: 'Overview',
-                            type: 'doc',
-                        },
-                        'app-runtime/components/DataQuery',
-                        'app-runtime/components/DataMutation',
-                    ],
-                },
-                {
-                    Types: [
-                        'app-runtime/types/Config',
-                        'app-runtime/types/Query',
-                        'app-runtime/types/Mutation',
-                    ],
-                },
-                {
-                    Advanced: [
-                        {
-                            'Offline Tools': [
+                            'CLI Scripts': [
                                 {
-                                    id: 'app-runtime/advanced/offline/README',
+                                    id: 'app-platform/scripts',
                                     label: 'Overview',
                                     type: 'doc',
                                 },
-                                'app-runtime/advanced/offline/CacheableSections',
-                                'app-runtime/advanced/offline/useDhis2ConnectionStatus',
-                                'app-runtime/advanced/offline/useOnlineStatus',
+                                {
+                                    id: 'app-platform/scripts/build',
+                                    label: 'build',
+                                    type: 'doc',
+                                },
+                                {
+                                    id: 'app-platform/scripts/deploy',
+                                    label: 'deploy',
+                                    type: 'doc',
+                                },
+                                {
+                                    id: 'app-platform/scripts/init',
+                                    label: 'init',
+                                    type: 'doc',
+                                },
+                                {
+                                    id: 'app-platform/scripts/pack',
+                                    label: 'pack',
+                                    type: 'doc',
+                                },
+                                {
+                                    id: 'app-platform/scripts/publish',
+                                    label: 'publish',
+                                    type: 'doc',
+                                },
+                                {
+                                    id: 'app-platform/scripts/start',
+                                    label: 'start',
+                                    type: 'doc',
+                                },
+                                {
+                                    id: 'app-platform/scripts/test',
+                                    label: 'test',
+                                    type: 'doc',
+                                },
                             ],
                         },
-                        'app-runtime/advanced/services',
-                        'app-runtime/advanced/DataEngine',
-                        'app-runtime/advanced/DataEngineLinks',
-                        'app-runtime/advanced/redux',
+                        {
+                            Configuration: [
+                                'app-platform/config/types',
+                                'app-platform/config/d2-config-js-reference',
+                                'app-platform/config/environment',
+                            ],
+                        },
+                        {
+                            Usage: [
+                                'app-platform/usage/dependencies',
+                                'app-platform/usage/css',
+                                'app-platform/usage/static-files',
+                                'app-platform/usage/app-icon',
+                            ],
+                        },
+                        'app-platform/pwa/pwa',
+                        'app-platform/architecture',
+                        'app-platform/troubleshooting',
+                        'app-platform/proxy',
+                        {
+                            id: 'app-platform/CHANGELOG',
+                            label: 'Changelog',
+                            type: 'doc',
+                        },
                     ],
                 },
                 {
-                    id: 'app-runtime/CHANGELOG',
-                    label: 'Changelog',
-                    type: 'doc',
-                },
-            ],
-        },
-        {
-            CLI: [
-                'cli/readme',
-                'cli/cli/commands/d2-overview',
-                {
-                    'd2 cluster': [
-                        'cli/cli/commands/d2-cluster',
-                        'cli/cli/recipes/stable-version',
-                        'cli/cli/recipes/development-version',
-                        'cli/cli/recipes/custom-dhis-config',
-                        'cli/cli/recipes/custom-docker-image',
-                        'cli/cli/commands/d2-utils-release',
+                    'Application Runtime': [
+                        'app-runtime/getting-started',
+                        'app-runtime/provider',
+                        {
+                            Hooks: [
+                                {
+                                    id: 'app-runtime/hooks/README',
+                                    label: 'Overview',
+                                    type: 'doc',
+                                },
+                                'app-runtime/hooks/useConfig',
+                                'app-runtime/hooks/useDataQuery',
+                                'app-runtime/hooks/useDataMutation',
+                                'app-runtime/hooks/useDataEngine',
+                                'app-runtime/hooks/useAlert',
+                                'app-runtime/hooks/useAlerts',
+                                'app-runtime/hooks/useTimeZoneConversion',
+                            ],
+                        },
+                        {
+                            Components: [
+                                {
+                                    id: 'app-runtime/components/README',
+                                    label: 'Overview',
+                                    type: 'doc',
+                                },
+                                'app-runtime/components/DataQuery',
+                                'app-runtime/components/DataMutation',
+                            ],
+                        },
+                        {
+                            Types: [
+                                'app-runtime/types/Config',
+                                'app-runtime/types/Query',
+                                'app-runtime/types/Mutation',
+                            ],
+                        },
+                        {
+                            Advanced: [
+                                {
+                                    'Offline Tools': [
+                                        {
+                                            id: 'app-runtime/advanced/offline/README',
+                                            label: 'Overview',
+                                            type: 'doc',
+                                        },
+                                        'app-runtime/advanced/offline/CacheableSections',
+                                        'app-runtime/advanced/offline/useDhis2ConnectionStatus',
+                                        'app-runtime/advanced/offline/useOnlineStatus',
+                                    ],
+                                },
+                                'app-runtime/advanced/services',
+                                'app-runtime/advanced/DataEngine',
+                                'app-runtime/advanced/DataEngineLinks',
+                                'app-runtime/advanced/redux',
+                            ],
+                        },
+                        {
+                            id: 'app-runtime/CHANGELOG',
+                            label: 'Changelog',
+                            type: 'doc',
+                        },
                     ],
                 },
-            ],
-        },
-        {
-            Cypress: [
                 {
-                    id: 'cli/cypress/getting-started',
-                    type: 'doc',
-                    label: 'Getting Started',
-                },
-                'cli/cypress/upgrade-guide',
-                {
-                    Guides: [
-                        'cli/cypress/guides/setting-up-cli-tool',
-                        'cli/cypress/guides/enable-auto-login',
-                        'cli/cypress/guides/using-cucumber',
-                        'cli/cypress/guides/using-the-network-shim',
-                        'cli/cypress/guides/cross-site-cookies',
-                        'cli/cypress/guides/custom-data-test-syntax',
-                        'cli/cypress/guides/add-login-credentials',
-                    ],
-                    'API: Commands': [
-                        'cli/cypress/commands/getWithDataTest',
-                        'cli/cypress/commands/findWithDataTest',
-                        'cli/cypress/commands/fillInLoginForm',
-                        'cli/cypress/commands/all',
-                    ],
-                    'API: Helper Functions': [
-                        'cli/cypress/helpers',
-                        'cli/cypress/helpers/dataTestNameToSelector',
-                        'cli/cypress/helpers/parseSelectorWithDataTest',
-                    ],
-                    'API: Setup': ['cli/cypress/setups/enable-auto-login'],
-                    'Developer Docs': [
-                        'cli/cypress/developer',
-                        'cli/cypress/developer/install',
-                        'cli/cypress/developer/network-shim',
-                        'cli/cypress/developer/troubleshooting',
+                    CLI: [
+                        'cli/readme',
+                        'cli/cli/commands/d2-overview',
+                        {
+                            'd2 cluster': [
+                                'cli/cli/commands/d2-cluster',
+                                'cli/cli/recipes/stable-version',
+                                'cli/cli/recipes/development-version',
+                                'cli/cli/recipes/custom-dhis-config',
+                                'cli/cli/recipes/custom-docker-image',
+                                'cli/cli/commands/d2-utils-release',
+                            ],
+                        },
                     ],
                 },
                 {
-                    id: 'cli/cypress/CHANGELOG',
-                    label: 'Changelog',
-                    type: 'doc',
+                    Cypress: [
+                        {
+                            id: 'cli/cypress/getting-started',
+                            type: 'doc',
+                            label: 'Getting Started',
+                        },
+                        'cli/cypress/upgrade-guide',
+                        {
+                            Guides: [
+                                'cli/cypress/guides/setting-up-cli-tool',
+                                'cli/cypress/guides/enable-auto-login',
+                                'cli/cypress/guides/using-cucumber',
+                                'cli/cypress/guides/using-the-network-shim',
+                                'cli/cypress/guides/cross-site-cookies',
+                                'cli/cypress/guides/custom-data-test-syntax',
+                                'cli/cypress/guides/add-login-credentials',
+                            ],
+                            'API: Commands': [
+                                'cli/cypress/commands/getWithDataTest',
+                                'cli/cypress/commands/findWithDataTest',
+                                'cli/cypress/commands/fillInLoginForm',
+                                'cli/cypress/commands/all',
+                            ],
+                            'API: Helper Functions': [
+                                'cli/cypress/helpers',
+                                'cli/cypress/helpers/dataTestNameToSelector',
+                                'cli/cypress/helpers/parseSelectorWithDataTest',
+                            ],
+                            'API: Setup': [
+                                'cli/cypress/setups/enable-auto-login',
+                            ],
+                            'Developer Docs': [
+                                'cli/cypress/developer',
+                                'cli/cypress/developer/install',
+                                'cli/cypress/developer/network-shim',
+                                'cli/cypress/developer/troubleshooting',
+                            ],
+                        },
+                        {
+                            id: 'cli/cypress/CHANGELOG',
+                            label: 'Changelog',
+                            type: 'doc',
+                        },
+                    ],
                 },
             ],
         },
