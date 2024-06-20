2960e3ef4b4b2423a1d4a2f86836e20121958d2a
Rene Pot <rene@dhis2.org>
2024-06-20 09:13:53 +0200

    fix: formatting and remove import react

--- src/MeetupAttendees/UserBadge.js
+++ src/MeetupAttendees/UserBadge.js
@@ -1,49 +1,44 @@
-import React, { Component } from 'react';
-import './badges.css';
+import { Component } from 'react'
+import './badges.css'
 
 class UserBadge extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isHovered: false };
-  }
+    constructor(props) {
+        super(props)
+        this.state = { isHovered: false }
+    }
 
-  handleMouseEnter = () => {
-    this.setState({ isHovered: true });
-  };
+    handleMouseEnter = () => {
+        this.setState({ isHovered: true })
+    }
 
-  handleMouseLeave = () => {
-    this.setState({ isHovered: false });
-  };
+    handleMouseLeave = () => {
+        this.setState({ isHovered: false })
+    }
 
-  render() {
-    const { link, img, username } = this.props;
-    const { isHovered } = this.state;
-    return (
-      <div
-        className="avatar badge"
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-      >
-        <a
-          className="avatar__photo-link avatar__photo badge-link"
-          target="_blank"
-          href={link}
-          onMouseEnter={this.handleMouseEnter}
-          onMouseLeave={this.handleMouseLeave}
-        >
-          <img
-            alt="Badge Winner"
-            src={img} />
-        </a>
-        <a
-          href={link}
-          target="_blank">
-          {username}
-        </a>
-      </div>
-
-    )
-  }
+    render() {
+        const { link, img, username } = this.props
+        const { isHovered } = this.state
+        return (
+            <div
+                className="avatar badge"
+                onMouseEnter={this.handleMouseEnter}
+                onMouseLeave={this.handleMouseLeave}
+            >
+                <a
+                    className="avatar__photo-link avatar__photo badge-link"
+                    target="_blank"
+                    href={link}
+                    onMouseEnter={this.handleMouseEnter}
+                    onMouseLeave={this.handleMouseLeave}
+                >
+                    <img alt="Badge Winner" src={img} />
+                </a>
+                <a href={link} target="_blank">
+                    {username}
+                </a>
+            </div>
+        )
+    }
 }
 
-export default UserBadge;
+export default UserBadge
