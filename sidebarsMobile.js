179d7ab867908254fc3c3c68320b33c2bd07ebeb
Victor Garcia <victor.garcia@dhis2.org>
2024-09-24 10:55:53 +0200

    Move Android SDK documentation to Mobile development

--- sidebarsMobile.js
+++ sidebarsMobile.js
@@ -8,6 +8,11 @@ module.exports = {
                     dirName: 'mobile-ui',
                 }
             ]
+        },
+        {
+            type: 'link',
+            label: 'Android SDK',
+            href: 'https://docs.dhis2.org/en/develop/developing-with-the-android-sdk/about-this-guide.html',
         }
     ],
 }
